a6b92f33482e92dde0f68da7ef4fbb6904c9c996
mariolenis <>
2017-05-13 16:31:41 -0500

    Refactor names of variables and remove the @Optional() decorator

--- _src/app/rethink/RethinkDB.service.ts
+++ _src/app/rethink/RethinkDB.service.ts
@@ -1,7 +1,8 @@
-import {Injectable, Optional} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {AngularRethinkDBObservable} from './RethinkDBObservable';
-import {IRethinkFilter, IRethinkDBAPIConfig} from './interfaces';
+import {IRethinkDBFilter, IRethinkDBAPIConfig} from './interfaces';
+import {Observable} from 'rxjs/Observable';
 
 export class RethinkDBAPIConfig implements IRethinkDBAPIConfig {
     api_key: string
@@ -13,24 +14,35 @@ export class RethinkDBAPIConfig implements IRethinkDBAPIConfig {
 @Injectable()
 export class AngularRethinkDBService {
     
-    private r: {db: string, table: string, r: Object}[] = [];
+    private r: {db: string, table: string, ngRDB: Object}[] = [];
 
-    constructor( @Optional() private config: RethinkDBAPIConfig, private http$: Http ) {}
+    constructor( private config: RethinkDBAPIConfig, private http$: Http ) {}
     
-    list(table: string, filter?: IRethinkFilter): AngularRethinkDBObservable<any[]> {
+    /**
+     * @description Function to initialize event listening on db.table. This function also
+     * stores a collection in memory every AngularRethinkDBObservable to avoid multiple creation 
+     * of listeners for db.table 
+     * 
+     * @param <string> table: name of table
+     * @param <Observable<IRethinkDBFilter>> filter$ : Filter to be applied to db.table events, 
+     * also will trigger a new query for every next value on filter$
+     * 
+     * @returns AngularRethinkDBObservable
+     */
+    list(table: string, filter$?: Observable<IRethinkDBFilter>): AngularRethinkDBObservable<any[]> {
         
-        let _r = this.r.filter(r => r.db === this.config.database && r.table === table)
-        if (_r.length === 0) { 
+        let ngRethinkDBObjects = this.r.filter(r => r.db === this.config.database && r.table === table)
+        if (ngRethinkDBObjects.length === 0) { 
                        
-            let r = new AngularRethinkDBObservable<any[]>(this.config, this.http$, table, filter);
+            let ngRethinkDBObject = new AngularRethinkDBObservable<any[]>(this.config, this.http$, table, filter$);
             this.r.push({
                 db: this.config.database,
                 table: table,
-                r: r
+                ngRDB: ngRethinkDBObject
             });
-            return r;
+            return ngRethinkDBObject;
             
-        } else
-            return _r.pop().r as AngularRethinkDBObservable<any[]>;
+        } else 
+            return ngRethinkDBObjects.pop().ngRDB as AngularRethinkDBObservable<any[]>;
     }
 }
