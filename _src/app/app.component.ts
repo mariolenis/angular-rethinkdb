1bf09b911bb7a7f8d58ceb89c510917d6599d893
mariolenis <>
2017-05-13 12:04:42 -0500

    Added auth observable

--- _src/app/app.component.ts
+++ _src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AngularRethinkDBService, AngularRethinkDBObservable} from './rethink/angular-rethinkdb';
 
 @Component({
