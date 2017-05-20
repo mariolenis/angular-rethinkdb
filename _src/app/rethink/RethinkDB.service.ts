9b0cb671b82ea0b32c624f52b83086d8783f208f
kerberus <kerberus@CTO-Kerberus>
2017-05-19 23:40:05 -0500

    Allow to have multiple sessions of table

--- _src/app/rethink/RethinkDB.service.ts
+++ _src/app/rethink/RethinkDB.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {AngularRethinkDBObservable} from './RethinkDBObservable';
-import {IRethinkDBFilter, IRethinkDBAPIConfig} from './interfaces';
+import {IRethinkDBQuery, IRethinkDBAPIConfig} from './interfaces';
 import {Observable} from 'rxjs/Observable';
 
 export class RethinkDBAPIConfig implements IRethinkDBAPIConfig {
@@ -14,35 +14,18 @@ export class RethinkDBAPIConfig implements IRethinkDBAPIConfig {
 @Injectable()
 export class AngularRethinkDBService {
     
-    private r: {db: string, table: string, ngRDB: Object}[] = [];
-
     constructor( private config: RethinkDBAPIConfig, private http$: Http ) {}
     
     /**
-     * @description Function to initialize event listening on db.table. This function also
-     * stores a collection in memory every AngularRethinkDBObservable to avoid multiple creation 
-     * of listeners for db.table 
+     * @description Function to initialize event listening on db.table. 
      * 
-     * @param <string> table: name of table
-     * @param <Observable<IRethinkDBFilter>> filter$ : Filter to be applied to db.table events, 
-     * also will trigger a new query for every next value on filter$
+     * @param <string> name of table
+     * @param <Observable<IRethinkDBQuery>> query to be applied to db.table events, 
+     * also will trigger a new query for every next value in query
      * 
-     * @returns AngularRethinkDBObservable
+     * @returns AngularRethinkDBObservable<any[]>
      */
-    list(table: string, filter$?: Observable<IRethinkDBFilter>): AngularRethinkDBObservable<any[]> {
-        
-        let ngRethinkDBObjects = this.r.filter(r => r.db === this.config.database && r.table === table)
-        if (ngRethinkDBObjects.length === 0) { 
-                       
-            let ngRethinkDBObject = new AngularRethinkDBObservable<any[]>(this.config, this.http$, table, filter$);
-            this.r.push({
-                db: this.config.database,
-                table: table,
-                ngRDB: ngRethinkDBObject
-            });
-            return ngRethinkDBObject;
-            
-        } else 
-            return ngRethinkDBObjects.pop().ngRDB as AngularRethinkDBObservable<any[]>;
+    list(table: string, query$?: Observable<IRethinkDBQuery>): AngularRethinkDBObservable<any[]> {        
+        return new AngularRethinkDBObservable<any[]>(this.config, this.http$, table, query$);
     }
 }
