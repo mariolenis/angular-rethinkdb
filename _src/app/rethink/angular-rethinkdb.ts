5adc3aabae2a122075d8826c7e947cfac94342a7
kerberus <kerberus@CTO-Kerberus>
2017-05-19 23:39:03 -0500

    Export RethinkDB Query interface

--- _src/app/rethink/angular-rethinkdb.ts
+++ _src/app/rethink/angular-rethinkdb.ts
@@ -1,3 +1,4 @@
 export {AngularRethinkDBModule} from './RethinkDB.module';
 export {AngularRethinkDBService} from './RethinkDB.service'
 export {AngularRethinkDBObservable} from './RethinkDBObservable';
+export {IRethinkDBQuery} from './interfaces';
