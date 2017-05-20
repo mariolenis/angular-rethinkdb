d23a1032213ac8acaec613106800a19fd6ffdfdf
kerberus <kerberus@CTO-Kerberus>
2017-05-19 23:40:43 -0500

    Test filters

--- _src/app/app.component.ts
+++ _src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {AngularRethinkDBService, AngularRethinkDBObservable} from './rethink/angular-rethinkdb';
+import {AngularRethinkDBService, AngularRethinkDBObservable, IRethinkDBQuery} from './rethink/angular-rethinkdb';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,25 @@ import {AngularRethinkDBService, AngularRethinkDBObservable} from './rethink/ang
 })
 export class AppComponent {
     title = 'Angular-RethinkDB works!';
-    messages: AngularRethinkDBObservable<{id: string, nombre: string, msg: string}[]>;
+    messages: AngularRethinkDBObservable<{id: string, nombre: string, msg: string, fecha: Date}[]>;
+    
+    query$ = new Subject<IRethinkDBQuery>();
+    filteredMessages: AngularRethinkDBObservable<{id: string, nombre: string, msg: string}[]>;
     
     constructor(public ar: AngularRethinkDBService) {
         this.messages = this.ar.list('counter');
+        this.filteredMessages = this.ar.list('counter', this.query$.asObservable());
+        
+        this.filteredMessages.subscribe(res => console.log(res));
+        
+        setTimeout(() => this.initFilter('Mario'), 250);
+    }
+    
+    initFilter(name: string) {
+        this.query$.next({
+            filter: {
+                nombre: name
+            }
+        });
     }
 }
