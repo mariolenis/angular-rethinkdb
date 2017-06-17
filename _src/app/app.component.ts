a9113aa6176c0e5e8ac92bfb6e1a962dcd302524
kerberus <kerberus@CTO-Kerberus>
2017-06-17 12:07:21 -0500

    Improve demo

--- _src/app/app.component.ts
+++ _src/app/app.component.ts
@@ -2,37 +2,40 @@ import {Component} from '@angular/core';
 import {AngularRethinkDBService, AngularRethinkDBObservable, IRethinkDBQuery} from './rethink/angular-rethinkdb';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
+import * as Hashids from 'Hashids';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    autor: string = "";
+    hashids: { encode: (x: number, y: number, z: number) => string };
+    
     title = 'Angular-RethinkDB works!';
     messages: AngularRethinkDBObservable<{id?: string, name: string, msg: string, date: Date}[]>;
     
     query$ = new BehaviorSubject<IRethinkDBQuery>(undefined);
     
     constructor(public ar: AngularRethinkDBService) {
-        this.messages = this.ar.list('counter');
         
-        this.initFilter('Mario');
+        // Create a visitor id
+        this.hashids = new Hashids('Angular-RethinkDB', 6, 'abcdefghijklmnopqrstuvwxyz123456789');
+        this.autor = 'visitor-' + this.hashids.encode(Math.round(Math.random() * 10), Math.round(Math.random() * 10), Math.round(Math.random() * 10));
         
-        this.ar.list('counter', this.query$)
-            .subscribe(res => console.log(res));
-    }
-    
-    initFilter(name: string) {
+        // Create an object attached to table 'chat'
+        this.messages = this.ar.list('chat', this.query$);
+        
+        // Filter | Order | limit
         this.query$.next({
-            filter: {
-                nombre: name
-            }
+            orderBy: 'date'
         });
     }
     
     sendMessage(msg: string): void {
         this.messages.push({
-            name: 'visitor',
+            name: this.autor,
             msg: msg,
             date: new Date()
         }).subscribe(res => console.log(res))      
